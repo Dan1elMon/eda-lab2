a0c353d9fc0577e79de9ef098f75c3a509d05a79
Daniel Chura <dcuhramo@unsa.edu.pe>
2023-05-17 13:49:49 -0500

    modificando ejercicioresuelto

---
+++ ejercicioResuelto.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ejercicioResuelto{
+	public static void main(String []args){
+		ArayList<String> alumnos=new ArrayList<String>();
+		ArayList<Integer> notas=new ArrayList<Integer>();
+		
+		alumnos.add("Henry");
+		alumnos.add("Diego");
+		alumnos.add("Richart");
+		alumnos.add("Gabriel");
+		System.out.println(alumnos.hashcode);
+		System.out.println(alumnos.IsEmpty);
+		System.out.println(alumnos.size);
+
+	}
+}
+
